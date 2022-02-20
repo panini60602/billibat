a0c8dcc74201091be8831ba34aae721a3cb295ca
panini <panini60602@gmail.com>
2022-02-21 01:11:55 +0900

    billibat-flutter_ボーラード完成

--- lib/main.dart
+++ lib/main.dart
@@ -3,62 +3,55 @@ import 'package:flutter/material.dart';
 
 import 'bowlard.dart';
 
-void main(){
+//メイン
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: const Header(),
           body: Stack(
-              alignment:Alignment.center,
-              children: const [
-                Image(image:AssetImage('images/top2.jpg'),fit:BoxFit.cover),
-
-                Positioned(
-                  top:20.0,
-                  width:300.0,
-                  height:50.0,
-                  child: Button01(),
-                ),
-              ],
+            alignment: Alignment.center,
+            children: const [
+              Image(image: AssetImage('images/top2.jpg'), fit: BoxFit.cover),
+              Positioned(
+                top: 20.0,
+                width: 300.0,
+                height: 50.0,
+                child: Button01(),
+              ),
+            ],
             fit: StackFit.expand,
-          )
-      ),
+          )),
     );
   }
 }
 
-
-class Button01 extends StatelessWidget{
+class Button01 extends StatelessWidget {
   const Button01({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-        child: const Text('ボーラード',style:TextStyle(fontSize:24)),
+      child: const Text('ボーラード', style: TextStyle(fontSize: 24)),
       style: ElevatedButton.styleFrom(
-          primary: Colors.grey[200],
-          onPrimary: Colors.black,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
+        primary: Colors.grey[200],
+        onPrimary: Colors.black,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
       ),
       onPressed: () {
-        Navigator.of(context).push(
-            MaterialPageRoute(
-                builder: (context){
-                  return const Bowlard();
-                }
-            )
-        );
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+          return const Bowlard();
+        }));
       },
     );
   }
